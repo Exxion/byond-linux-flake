d34e532e5a5ec8274ada9488bc1190237afc56b9
Exxion <exxion191@gmail.com>
2022-11-04 01:03:16 -0400

    515.1594

--- flake.nix
+++ flake.nix
@@ -3,11 +3,11 @@
 
   inputs.byond-linux = {
     type = "tarball";
-    url = "https://www.byond.com/download/build/515/515.1593_byond_linux.zip";
+    url = "https://www.byond.com/download/build/515/515.1594_byond_linux.zip";
     flake = false;
   };
 
-  outputs = { self, nixpkgs, byond-linux, ... }: let byond_ver = "515"; byond_build = "1593"; in rec {
+  outputs = { self, nixpkgs, byond-linux, ... }: let byond_ver = "515"; byond_build = "1594"; in rec {
     packages.i686-linux.byond-linux = with import nixpkgs { config.allowUnfree = true; system = "i686-linux"; };
       stdenv.mkDerivation rec {
         pname = "byond-linux";
